218fd0656026a8cdcd303e3b02f808dd34570f6f
Набоков Сергей Анатольевич <sanabokov@sberbank.ru>
2022-05-04 14:31:38 +0300

    otp

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,29 @@ import React, {useEffect} from 'react';
 import './App.css';
 
 function App() {
-  useEffect(() => {
-
-  }, []);
-  return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        if ('OTPCredential' in window) {
+            const ac = new AbortController();
+            navigator.credentials.get({
+                // @ts-ignore
+                otp: {transport: ['sms']},
+                signal: ac.signal
+            }).then(otp => {
+                // @ts-ignore
+                console.log({code});
+                ac.abort();
+            }).catch(err => {
+                console.log(err);
+            });
+        }
+    }, []);
+    return (
+        <div className="App">
+            <header className="App-header">
+                OTP
+            </header>
+        </div>
+    );
 }
 
 export default App;
